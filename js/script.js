644429bcc4974f9b0f0eff163cbd0d6e4d557067
Matthew! <matthewgreat92@gmail.com>
2021-09-14 12:10:33 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// alert("Hello MatherFacker");
+
+// const result = confirm("Ты выучил JS, преждже чем сесть играть?");
+// console.log(result);
+
+const answer = prompt("Вам есть 18 лет?", "18");
+console.log(answer);
