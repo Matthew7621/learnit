ee0e69aac12051c7526693f36fce456a68235109
Matthew7621 <matthewgreat92@gmail.com>
2021-09-14 12:52:53 +0300

    i dont now, why this file do not add early

--- js/script.js
+++ js/script.js
@@ -5,5 +5,10 @@
 // const result = confirm("Ты выучил JS, преждже чем сесть играть?");
 // console.log(result);
 
-const answer = prompt("Вам есть 18 лет?", "18");
-console.log(answer);
+// const answer = prompt("Вам есть 18 лет?", "18");
+// console.log(answer);
+
+const quest1 = 2 + 2 * 2,
+      quest2 = '6';
+
+console.log(quest1 == quest2);
